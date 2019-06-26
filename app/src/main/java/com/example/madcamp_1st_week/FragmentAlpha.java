5bc71abda73050210c37ca5c4ef2ec01f94d6ed3
BIGPEL <bigpel66@gmail.com>
2019-06-26 15:51:57 +0900

    first commit

---
+++ app/src/main/java/com/example/madcamp_1st_week/FragmentAlpha.java
@@ -0,0 +1,23 @@
+package com.example.madcamp_1st_week;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class FragmentAlpha extends Fragment {
+    View view;
+    public FragmentAlpha() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.alpha_fragment, container, false);
+        return view;
+    }
+}
---
+++ app/src/main/java/com/example/madcamp_1st_week/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.madcamp_1st_week;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.appbar.AppBarLayout;
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private AppBarLayout appBarLayout;
+    private ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tabLayout = (TabLayout) findViewById(R.id.tablayout_id);
+        appBarLayout = (AppBarLayout) findViewById(R.id.appbar_id);
+        viewPager = (ViewPager) findViewById(R.id.viewpager_id);
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        // Adding Fragments
+        adapter.AddFragment(new FragmentContacts(), "Contacts");
+        adapter.AddFragment(new FragmentGallery(), "Gallery");
+        adapter.AddFragment(new FragmentAlpha(), "Alpha");
+        // adapter Setup
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+}
